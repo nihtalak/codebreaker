d84b0c69542cfeef63ea519005a0e9091a8ac14e
Vladimir Oberemok <nihtalak@gmail.com>
2013-05-27 19:43:51 -0700

    Code-breaker submits guess userstory done

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -4,11 +4,30 @@ module Codebreaker
   class Game
     def initialize(output = $stdout)
       @output = output   
+      @secret_code = make_secret_code
     end
 
     def start
       @output.puts("Welcome to Codebreaker!")
       @output.puts("Enter first guess:")
     end  
+
+    def guess(number)
+      match_array = @secret_code.chars.zip(number.chars)
+      match_array.take(number.length).each do |origin, guessed|
+        if origin == guessed
+          @output.print("+")
+        elsif @secret_code.include?(guessed)
+          @output.print("-")
+        end
+      end
+    end
+
+    private
+    def make_secret_code(n = 4)
+      str = ""
+      n.times { str << rand(1..6).to_s }
+      str
+    end
   end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
-    describe "#start" do
-      let (:output) { mock('output').as_null_object }
-      let (:game) { Game.new(output) }
+    let (:output) { mock('output').as_null_object }
+    let (:game) { Game.new(output) }
 
+    describe "#start" do
       it "sends welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start
@@ -14,6 +14,31 @@ module Codebreaker
       it "prompts the first guess" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start
+      end
+    end
+
+    describe "#guess" do
+      before(:each) { game.instance_variable_set(:@secret_code, "1234") }
+
+      it "sends '+' if number is exactly guessed" do
+        output.should_receive(:print).with("+").once
+        game.guess("1")
+      end
+
+      it "sends '-' if number is guessed but in different position" do
+        output.should_receive(:print).with("-").once
+        game.guess("3")
+      end
+
+      it "sends nothing if secret code does not include that number" do
+        output.should_not_receive(:print)
+        game.guess("5")
+      end
+
+      it "sends '+-+' if 2 numbers exactly guessed and 1 include in secret code" do
+        output.should_receive(:print).with("+").twice
+        output.should_receive(:print).with("-").once
+        game.guess("1644")
       end
     end
   end
